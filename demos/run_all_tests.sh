997a5e0ffa495c576e77e07c778bafb4c7dc2f08
Yangkang Chen <chenyk1990@gmail.com>
2022-10-29 23:45:59 -0500

    add run_all_tests.sh

---
+++ demos/run_all_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+svn co https://github.com/aaspip/data/trunk ./data #or git clone https://github.com/aaspip/data
+cp -rf ./data/* ./
+
+python test_pyseistr_somean2d.py
+python test_pyseistr_somean3d.py
+python test_pyseistr_somf2d.py
+python test_pyseistr_somf3d.py
+
+
+python test_pyseistr_passive_recon3d.py
+python test_pyseistr_ssprecursor.py
+python test_pyseistr_rf.py
+python test_pyseistr_das.py
