bb8a117ec0b3f5c5cb4a906ef236a14f2857aebc
Yangkang Chen <chenyk1990@gmail.com>
2023-04-26 14:56:54 -0500

    up

--- demos/test_pyseistr_das_massive.py
+++ demos/test_pyseistr_das_massive.py
@@ -152,7 +152,7 @@ fname="FORGE/FORGE_78-32_iDASv3-P11_UTC190423213209.sgy"
 clip=20;
 nwin=3000
 thr=0.5
-v=np.linspace(-0.00013,0.00013,100);
+v=np.linspace(-0.00023,0.00023,100);
 nt=nwin;nx=960;dt=0.0005;
 h=np.linspace(0,nx-1,nx)
 par={'v':v,'nt':nt,'h':h,'dt':dt,'typ':1,'oper':1}
@@ -162,7 +162,7 @@ files=["FORGE_78-32_iDASv3-P11_UTC190423213209.sgy"]
 import glob
 lines=glob.glob('FORGE/*.sgy')
 files=[ii.split('/')[-1] for ii in lines]
-files=files[1:]
+files=files[:]
 for ii in range(len(files)):
 	ifile=files[ii]
 	print(ii,'/',len(files),ifile)
@@ -183,7 +183,6 @@ for ii in range(len(files)):
 	## Cmax parameter
 	h=np.linspace(0,nx-1,nx);
 	par={'v':v,'nt':nwin,'h':h,'dt':dt,'typ':1,'oper':1};
-	print(par)
 	
 	ic=0
 	t0=-nwin;
@@ -193,15 +192,11 @@ for ii in range(len(files)):
 		d0=dn[t0:t0+nwin,:]
 		if nwin != d0.shape[0]:
 			print('nwin=',nwin,'n1=',d0.shape[0])
-		d1=ps.bandpassc(d0,0.0005,0,200,6,6,0,0);d1_bp=d1.copy(); 		##BP
-# 		d1=d0;
-# 		d1_bp=d1;#d1_bp=d1.copy();
-		pp=ps.dip2dc(d1,2,10,2,0.01, 1, 0.000001,[40,40,1],verb=0); 			##SOMF
-# 		print('d1,pp shape:',d1.shape,pp.shape)
-		d1=ps.somf2dc(d1,pp,8,2,0.01,verb=0);d1_bpsomf=d1.copy();			#SOMF
-# 		d1_bpsomf=d1;#d1_bpsomf=d1.copy();
-		d1=d1-ps.fkdip(d1,0.02);d1_bpsomffk=d1.copy()			#FK
-# 		d1_bpsomffk=d1;#d1_bpsomffk=d1.copy()
+		d1=d0;
+# 		d1=ps.bandpassc(d0,0.0005,0,200,6,6,0,0);d1_bp=d1.copy(); 	##BP
+# 		pp=ps.dip2dc(d1,2,10,2,0.01, 1, 0.000001,[40,40,1],verb=0); ##SOMF
+# 		d1=ps.somf2dc(d1,pp,8,2,0.01,verb=0);d1_bpsomf=d1.copy();	#SOMF
+# 		d1=d1-ps.fkdip(d1,0.02);d1_bpsomffk=d1.copy()				#FK
 		c=ps.cohc(d1,par);
 		cmax=c.max()
 		print('Window',ic,' Cmax=',cmax)
@@ -211,26 +206,28 @@ for ii in range(len(files)):
 			fig = plt.figure(figsize=(6, 8))
 			ax=plt.subplot(3,2,1)
 			plt.imshow(d0,cmap=seis(),clim=(-clip,clip),aspect='auto');ax.set_xticks([]);ax.set_yticks([]);
-			plt.title('Raw DAS data');
-			ax=plt.subplot(3,2,3)
-			plt.imshow(d1_bp,cmap=seis(),clim=(-clip,clip),aspect='auto');ax.set_xticks([]);ax.set_yticks([]);
-			plt.title('BP');
-			ax=plt.subplot(3,2,4)
-			plt.imshow(d1_bpsomf,cmap=seis(),clim=(-clip,clip),aspect='auto');ax.set_xticks([]);ax.set_yticks([]);
-			plt.title('BPSOMF');
-			ax=plt.subplot(3,2,5)
-			plt.imshow(d1_bpsomffk,cmap=seis(),clim=(-clip,clip),aspect='auto');ax.set_xticks([]);ax.set_yticks([]);
-			plt.title('BPSOMFFK (Cmax=%g)'%cmax);
-			ax=plt.subplot(3,2,6)
-			plt.imshow(d0-d1_bpsomffk,cmap=seis(),clim=(-clip,clip),aspect='auto');ax.set_xticks([]);ax.set_yticks([]);
-			plt.title('Removed Noise');
+# 			plt.title('Raw DAS data');
+			plt.title(ifile+' '+str(t0),fontsize=20)
+			plt.xlabel('Cmax=%g'%cmax)
+# 			ax=plt.subplot(3,2,3)
+# 			plt.imshow(d1_bp,cmap=seis(),clim=(-clip,clip),aspect='auto');ax.set_xticks([]);ax.set_yticks([]);
+# 			plt.title('BP');
+# 			ax=plt.subplot(3,2,4)
+# 			plt.imshow(d1_bpsomf,cmap=seis(),clim=(-clip,clip),aspect='auto');ax.set_xticks([]);ax.set_yticks([]);
+# 			plt.title('BPSOMF');
+# 			ax=plt.subplot(3,2,5)
+# 			plt.imshow(d1_bpsomffk,cmap=seis(),clim=(-clip,clip),aspect='auto');ax.set_xticks([]);ax.set_yticks([]);
+# 			plt.title('BPSOMFFK (Cmax=%g)'%cmax);
+# 			ax=plt.subplot(3,2,6)
+# 			plt.imshow(d0-d1_bpsomffk,cmap=seis(),clim=(-clip,clip),aspect='auto');ax.set_xticks([]);ax.set_yticks([]);
+# 			plt.title('Removed Noise');
 			plt.savefig('FORGEFIG/%s_%d_%d.png'%(ifile,ic,t0),format='png',dpi=200)
 			plt.close;
 		del d0
 		del d1
-		del d1_bp
-		del d1_bpsomf
-		del d1_bpsomffk
+# 		del d1_bp
+# 		del d1_bpsomf
+# 		del d1_bpsomffk
 		del c
 	del dn
 	del d
