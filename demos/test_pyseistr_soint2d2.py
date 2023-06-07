9e829eb8d14e8fd0aa33c1e10485eefe99c63452
Yangkang Chen <chenyk1990@gmail.com>
2023-06-06 23:13:22 -0500

    add more

--- demos/test_pyseistr_soint2d2.py
+++ demos/test_pyseistr_soint2d2.py
@@ -1,31 +1,37 @@
-#This is example showing how to calculate RGT using pyseistr
+#This is another SOINT example by spraying the trace along the seismic slope#
 from pyseistr import smooth
 from pyseistr import dip2dc
-from pyseistr import rgt
 import matplotlib.pyplot as plt;
-
 from pyseistr import sigmoid
 sig=sigmoid(n1=200,n2=210);
 sig=smooth(sig,rect=[3,1,1],diff=[1,0,0],adj=0);
 sig=smooth(sig,rect=[3,1,1])
 
 sdip=dip2dc(sig,order=2,niter=10,rect=[4,4,1],verb=0)
-time=rgt(sdip,o1=0,d1=0.004,order=2,i0=50,eps=0.1);
+import numpy as np
+from pyseistr import pwpaintc
+data=np.zeros([sdip.shape[0],sdip.shape[1]])
+data[:,50]=sig[:,50];
+flat=pwpaintc(sdip,sig[:,50],order=2,i0=50,eps=0.1);
+# plt.imshow(flat);plt.show();
 
 fig = plt.figure(figsize=(12, 6))
 ax=plt.subplot(1,3,1)
-plt.imshow(sig,cmap='gray',clim=(-0.02, 0.02),aspect='auto');ax.set_xticks([]);ax.set_yticks([]);plt.title('Seismic');
+plt.imshow(data,cmap='gray',clim=(-0.01, 0.01),aspect='auto');ax.set_xticks([]);ax.set_yticks([]);plt.title('Sparse Seismic');
 plt.colorbar(orientation='horizontal');
 ax=plt.subplot(1,3,2)
 plt.imshow(sdip,cmap='jet',clim=(-2, 2),aspect='auto');ax.set_xticks([]);ax.set_yticks([]);plt.title('Slope');
 plt.colorbar(orientation='horizontal');
 ax=plt.subplot(1,3,3)
-plt.imshow(time,cmap='jet',clim=(0, 0.7),aspect='auto');ax.set_xticks([]);ax.set_yticks([]);plt.title('RGT');
+plt.imshow(flat,cmap='gray',clim=(-0.02, 0.02),aspect='auto');ax.set_xticks([]);ax.set_yticks([]);plt.title('Reconstructed');
 plt.colorbar(orientation='horizontal');
 
-plt.savefig('test_pyseistr_rgt2d.png',format='png',dpi=300)
+plt.savefig('test_pyseistr_soint2d2.png',format='png',dpi=300)
 plt.show()
 
+	
+	
+	
 
 
 
