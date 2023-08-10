d2d7188b0ee185bce8eb23db3a04d20b94917371
Yangkang Chen <chenyk1990@gmail.com>
2023-08-09 21:03:17 -0500

    up

--- pyseistr/plot.py
+++ pyseistr/plot.py
@@ -3,6 +3,19 @@ from matplotlib.colors import ListedColormap
 import numpy as np
 
 def cseis():
+	'''
+	cseis: seismic colormap
+	
+	By Yangkang Chen
+	June, 2022
+	
+	EXAMPLE
+	from pyseistr import cseis
+	import numpy as np
+	from matplotlib import pyplot as plt
+	plt.imshow(np.random.randn(100,100),cmap=cseis())
+	plt.show()
+	'''
 	seis=np.concatenate(
 (np.concatenate((0.5*np.ones([1,40]),np.expand_dims(np.linspace(0.5,1,88),axis=1).transpose(),np.expand_dims(np.linspace(1,0,88),axis=1).transpose(),np.zeros([1,40])),axis=1).transpose(),
 np.concatenate((0.25*np.ones([1,40]),np.expand_dims(np.linspace(0.25,1,88),axis=1).transpose(),np.expand_dims(np.linspace(1,0,88),axis=1).transpose(),np.zeros([1,40])),axis=1).transpose(),
